e7a27d5298446a4eff12be7a88f4d70d1da1836c
maritomunoz <marchim12@hotmail.com>
2015-09-04 15:36:35 -0400

    base de mapa

--- js/controllers/index.js
+++ js/controllers/index.js
@@ -1,10 +1,28 @@
-angular.module('myApp.controllers.index', [])
+angular.module('myApp.controllers.index', ["uiGmapgoogle-maps"])
 
 .controller('index#index', ['$scope', function ($scope) {
 	console.log("index#index running")
 
 	$scope.testing = "The Testing Text"
-
+	$scope.map = { center: { latitude: -39.8128564, longitude: -73.2469609 }, zoom: 14 };
+	$scope.markers = [
+		{
+			id: 0,
+			coords: {
+				latitude: -39.8128564,
+				longitude: -73.2469609
+			},
+			options: { draggable: false },
+		},
+		{
+			id: 1,
+			coords: {
+				latitude: -39.81647017616702,
+				longitude: -73.24259150120243
+			},
+			options: { draggable: false },
+		}
+	]
 }])
 
 
