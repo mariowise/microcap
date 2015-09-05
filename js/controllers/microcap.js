18e296fdff33dd560fb3ae4f75e8a710e0925aac
maritomunoz <marchim12@hotmail.com>
2015-09-05 03:06:56 -0400

    estrellita donde estas, me pregunto quien serás

--- js/controllers/microcap.js
+++ js/controllers/microcap.js
@@ -24,6 +24,16 @@ angular.module('myApp.controllers.microcap', [])
 			$scope.courses = _.filter(courses, function (item) { return item.labels.indexOf(label) != -1 })
 		}
 	}
+	setTimeout(
+		function() 
+		{
+			$(".rating").each(function(){
+				$(this).raty({ starType: 'i',
+					readOnly : true,
+					score: $(this).attr("data-score"),
+				});
+			})
+		}, 50);
 })
 
 .controller('microcap#show', function ($scope, $stateParams, Microcap) {
@@ -33,8 +43,7 @@ angular.module('myApp.controllers.microcap', [])
 	console.log($scope.course)
 
 	$('.rating').raty({ starType: 'i',
-						readOnly : true, 
-						score: $scope.course.user.score,
-						
-					});
+		readOnly : true, 
+		score: $scope.course.user.score,
+	});
 })
