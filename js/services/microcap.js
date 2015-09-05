b1053049da4f6edf5c8acb07a10356be0c93428d
maritomunoz <marchim12@hotmail.com>
2015-09-05 11:18:16 -0400

    .

--- js/services/microcap.js
+++ js/services/microcap.js
@@ -4,38 +4,52 @@ angular.module('myApp.services.microcap', [])
 	var db = [
 		{
 			id: 1,
-			title: "Curso de Inglés",
-			description: "Curso para aprender inglés básico gratuito",
-			date: "01/07/2016",
-			labels: ["Inglés", "Curso Municipal"],
-			image_url: "img/danilo.png",
-			tarifa: "-",
+			title: "Curso de Matemáticas PSU",
+			description: "Curso para fortalecer los conocimientos de PSU generales",
+			date: "20/08/2016",
+			labels: ["PSU"],
+			image_url: "img/diegoj.jpg",
+			tarifa: "15.000 pesos por clase, maximo de 5 personas",
 			hora:"Despues de las 7pm",
 			user: {
-				username: "Danilo Aburto",
-				score: 2
+				username: "Diego Jaume",
+				score: 1
 			}
 		},
 		{
 			id: 2,
 			title: "Taller de carpintería",
-			description: "Hagalo usted mismo, carpintería en el hogar",
+			description: "Hagalo usted mismo, carpintería en el hogar (Organizado por Muebles Lopez.)",
 			date: "02/07/2016",
 			labels: ["Técnico"],
-			image_url: "img/daniel.jpg",
+			image_url: "https://pbs.twimg.com/profile_images/632607891703382016/-lL_bc1L_400x400.jpg",
 			tarifa: "15.000 pesos por clase, maximo de 5 personas",
 			hora:"Despues de las 7pm",
 			user: {
-				username: "Daniel Quinteros",
+				username: "Mario Lopez L.",
 				score: 4
 			}
 		},
 		{
 			id: 3,
-			title: "Curso de computación",
-			description: "Nunca es tarde para aprender computación, inscríbete en este curso básico",
+			title: "Curso de Redes sociales",
+			description: "Nunca es tarde para aprender redes sociales, inscríbete en este curso básico",
 			date: "03/07/2016",
-			labels: ["Computación", "Informática"],
+			labels: ["Computación", "Informática", "Twitter"],
+			image_url: "https://pbs.twimg.com/profile_images/622875213475250177/8JIs8AYU_400x400.png",
+			tarifa: "15.000 pesos por clase, maximo de 5 personas",
+			hora:"Despues de las 7pm",
+			user: {
+				username: "Fernando Rodríguez",
+				score: 5
+			}
+		},
+		{
+			id: 4,
+			title: "Taller de Programación en Rails",
+			description: "Curso para profundizar sobre el framework Ruby on Rails Empresarial",
+			date: "08/09/2016",
+			labels: ["Cine"],
 			image_url: "img/mariom.jpg",
 			tarifa: "15.000 pesos por clase, maximo de 5 personas",
 			hora:"Despues de las 7pm",
@@ -45,7 +59,21 @@ angular.module('myApp.services.microcap', [])
 			}
 		},
 		{
-			id: 4,
+			id: 5,
+			title: "Taller de Teatro",
+			description: "Taller para iniciarse en el mundo del teatro",
+			date: "22/08/2016",
+			labels: ["Teatro"],
+			image_url: "img/rai.jpg",
+			tarifa: "15.000 pesos por clase, maximo de 5 personas",
+			hora:"Despues de las 7pm",
+			user: {
+				username: "Raimundo Fernandez",
+				score: 3
+			}
+		},
+		{
+			id: 6,
 			title: "Taller de emprendimiento",
 			description: "Taller que trata los temas fundamentales para iniciar un emprendimiento",
 			date: "03/07/2016",
@@ -56,34 +84,6 @@ angular.module('myApp.services.microcap', [])
 			user: {
 				username: "Diego Berrios",
 				score: 3
-			}
-		},
-		{
-			id: 5,
-			title: "Taller de cocina chilena",
-			description: "Aquí podras conocer las distintas recetas mas ricas de la cocina chilena",
-			date: "07/08/2016",
-			labels: ["Cocina", "Curso Municipal"],
-			image_url: "img/gab.jpg",
-			tarifa: "15.000 pesos por clase, maximo de 5 personas",
-			hora:"Despues de las 7pm",
-			user: {
-				username: "Gabriela León",
-				score: 3
-			}
-		},
-		{
-			id: 6,
-			title: "Curso de Matemáticas",
-			description: "Curso para aprender fracciones desde cero",
-			date: "10/08/2016",
-			labels: ["Matemáticas"],
-			image_url: "img/gonzalo.jpg",
-			tarifa: "15.000 pesos por clase, maximo de 5 personas",
-			hora:"Despues de las 7pm",
-			user: {
-				username: "Gonzalo Barra",
-				score: 4
 			}
 		},
 		{
@@ -102,29 +102,29 @@ angular.module('myApp.services.microcap', [])
 		},
 		{
 			id: 8,
-			title: "Curso de Matemáticas PSU",
-			description: "Curso para fortalecer los conocimientos de PSU generales",
-			date: "20/08/2016",
-			labels: ["PSU"],
-			image_url: "img/diegoj.jpg",
-			tarifa: "15.000 pesos por clase, maximo de 5 personas",
+			title: "Curso de Inglés",
+			description: "Curso para aprender inglés básico gratuito",
+			date: "01/07/2016",
+			labels: ["Inglés", "Curso Públicos"],
+			image_url: "img/danilo.png",
+			tarifa: "-",
 			hora:"Despues de las 7pm",
 			user: {
-				username: "Diego Jaume",
-				score: 1
+				username: "Danilo Aburto",
+				score: 2
 			}
 		},
 		{
 			id: 9,
-			title: "Taller de Teatro",
-			description: "Taller para iniciarse en el mundo del teatro",
-			date: "22/08/2016",
-			labels: ["Teatro"],
-			image_url: "img/rai.jpg",
+			title: "Taller de cocina chilena",
+			description: "Aquí podras conocer las distintas recetas mas ricas de la cocina chilena",
+			date: "07/08/2016",
+			labels: ["Cocina", "Curso Públicos"],
+			image_url: "img/gab.jpg",
 			tarifa: "15.000 pesos por clase, maximo de 5 personas",
 			hora:"Despues de las 7pm",
 			user: {
-				username: "Raimundo Fernandez",
+				username: "Gabriela León",
 				score: 3
 			}
 		},
@@ -186,16 +186,16 @@ angular.module('myApp.services.microcap', [])
 		},
 		{
 			id: 14,
-			title: "Taller de Programación en Rails",
-			description: "Curso para profundizar sobre el framework Rails",
-			date: "08/09/2016",
-			labels: ["Cine"],
-			image_url: "img/mariom.jpg",
+			title: "Curso de Matemáticas",
+			description: "Curso para aprender fracciones desde cero",
+			date: "10/08/2016",
+			labels: ["Matemáticas"],
+			image_url: "img/gonzalo.jpg",
 			tarifa: "15.000 pesos por clase, maximo de 5 personas",
 			hora:"Despues de las 7pm",
 			user: {
-				username: "Mario Muñoz",
-				score: 5
+				username: "Gonzalo Barra",
+				score: 4
 			}
 		},
 		{
