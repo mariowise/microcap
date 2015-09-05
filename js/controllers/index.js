b0e894de6fac540ad410f43dc2e19d3a6d94a977
maritomunoz <marchim12@hotmail.com>
2015-09-05 00:22:02 -0400

    ver curso

--- js/controllers/index.js
+++ js/controllers/index.js
@@ -1,8 +1,6 @@
 angular.module('myApp.controllers.index', ["uiGmapgoogle-maps"])
 
 .controller('index#index', ['$scope', 'Microcap', function ($scope, Microcap) {
-	
-
 	$scope.courses = Microcap.all()
 }])
 
